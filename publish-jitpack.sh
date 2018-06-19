5feec00afe9ebb82d140c152325e6e794e2af387
zhxnlai <zhxnlai@gmail.com>
2018-06-19 20:10:57 +0200

    Support Jitpack

---
+++ publish-jitpack.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+./gradlew clean
+
+./gradlew scarlet-core:build scarlet-core:publishToMavenLocal
+
+./gradlew scarlet-core-internal:build scarlet-core-internal:publishToMavenLocal
+
+./gradlew state-machine:build state-machine:publishToMavenLocal
+
+./gradlew scarlet:build scarlet:publishToMavenLocal
+
+./gradlew scarlet-test-utils:build scarlet-test-utils:publishToMavenLocal
+
+./gradlew scarlet-message-adapter-builtin:build scarlet-message-adapter-builtin:publishToMavenLocal
+
+./gradlew scarlet-message-adapter-protobuf:build scarlet-message-adapter-protobuf:publishToMavenLocal
+
+./gradlew scarlet-stream-adapter-builtin:build scarlet-stream-adapter-builtin:publishToMavenLocal
+
+./gradlew scarlet-stream-adapter-rxjava2:build scarlet-stream-adapter-rxjava2:publishToMavenLocal
+
+./gradlew scarlet-websocket-okhttp:build scarlet-websocket-okhttp:publishToMavenLocal
+
+./gradlew scarlet-websocket-mockwebserver:build scarlet-websocket-mockwebserver:publishToMavenLocal
+
+./gradlew scarlet-lifecycle-android:assemble scarlet-lifecycle-android:generatePomFileForAarPublication
+./gradlew scarlet-lifecycle-android:publishToMavenLocal
