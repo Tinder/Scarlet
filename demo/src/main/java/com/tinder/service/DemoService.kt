9233f4cb72f4ec87de71ee26b110e2c08872a4c9
zhxnlai <zhxnlai@gmail.com>
2019-01-17 12:59:01 -0800

    F

---
+++ demo/src/main/java/com/tinder/service/DemoService.kt
@@ -0,0 +1,16 @@
+/*
+ * © 2018 Match Group, LLC.
+ */
+
+package com.tinder.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class DemoService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
