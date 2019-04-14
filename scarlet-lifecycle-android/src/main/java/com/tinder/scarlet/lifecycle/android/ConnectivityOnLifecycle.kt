48776ff07187046e0d2ddc3e588ce3b79224711b
KienHT <kienhantrung@gmail.com>
2019-04-14 17:22:49 +0700

    unregister connectivity broadcast receiver when lifecycleState = Completed

--- scarlet-lifecycle-android/src/main/java/com/tinder/scarlet/lifecycle/android/ConnectivityOnLifecycle.kt
+++ scarlet-lifecycle-android/src/main/java/com/tinder/scarlet/lifecycle/android/ConnectivityOnLifecycle.kt
@@ -4,6 +4,7 @@
 
 package com.tinder.scarlet.lifecycle.android
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -14,9 +15,11 @@ import com.tinder.scarlet.LifecycleState
 import com.tinder.scarlet.lifecycle.LifecycleRegistry
 
 internal class ConnectivityOnLifecycle(
-    applicationContext: Context,
-    private val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry()
+        private val applicationContext: Context,
+        private val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry()
 ) : Lifecycle by lifecycleRegistry {
+
+    private val connectivityChangeBroadcastReceiver = ConnectivityChangeBroadcastReceiver()
 
     init {
         emitCurrentConnectivity(applicationContext)
@@ -25,16 +28,16 @@ internal class ConnectivityOnLifecycle(
 
     private fun emitCurrentConnectivity(applicationContext: Context) {
         val connectivityManager =
-            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         lifecycleRegistry.onNext(toLifecycleState(connectivityManager.isConnected()))
     }
 
     private fun subscribeToConnectivityChange(applicationContext: Context) {
-        val intentFilter = IntentFilter()
-            .apply { addAction(ConnectivityManager.CONNECTIVITY_ACTION) }
-        applicationContext.registerReceiver(ConnectivityChangeBroadcastReceiver(), intentFilter)
+        val intentFilter = IntentFilter().apply { addAction(ConnectivityManager.CONNECTIVITY_ACTION) }
+        applicationContext.registerReceiver(connectivityChangeBroadcastReceiver, intentFilter)
     }
 
+    @SuppressLint("MissingPermission")
     private fun ConnectivityManager.isConnected(): Boolean {
         val activeNetworkInfo = activeNetworkInfo
         return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting
@@ -46,6 +49,10 @@ internal class ConnectivityOnLifecycle(
         LifecycleState.Stopped
     }
 
+    fun unregisterReceiver() {
+        applicationContext.unregisterReceiver(connectivityChangeBroadcastReceiver)
+    }
+
     private inner class ConnectivityChangeBroadcastReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             val extras = intent.extras ?: return
@@ -54,3 +61,4 @@ internal class ConnectivityOnLifecycle(
         }
     }
 }
+
--- scarlet/src/main/java/com/tinder/scarlet/lifecycle/FlowableLifecycle.kt
+++ scarlet/src/main/java/com/tinder/scarlet/lifecycle/FlowableLifecycle.kt
@@ -8,14 +8,42 @@ import com.tinder.scarlet.Lifecycle
 import com.tinder.scarlet.LifecycleState
 import io.reactivex.Flowable
 import org.reactivestreams.Publisher
+import java.lang.reflect.Method
 
 internal class FlowableLifecycle(
-    private val flowable: Flowable<LifecycleState>
+        private val flowable: Flowable<LifecycleState>
 ) : Lifecycle, Publisher<LifecycleState> by flowable {
+
+    companion object {
+        const val CONNECTIVITY_ON_LIFECYCLE_CLASS_NAME = "com.tinder.scarlet.lifecycle.android.ConnectivityOnLifecycle"
+        const val UNREGISTER_CONNECTIVITY_BROADCAST_RECEIVER_METHOD_NAME = "unregisterReceiver"
+    }
 
     override fun combineWith(others: List<Lifecycle>): Lifecycle {
         val lifecycles = others + this
-        val flowable = Flowable.combineLatest(lifecycles) { it.map { it as LifecycleState }.combine() }
+
+        val flowable =
+                Flowable.combineLatest(lifecycles) { lifecycle ->
+                    val state = lifecycle.map { it as LifecycleState }.combine()
+                    lifecycles.unregisterConnectivityBroadcastReceiver(state)
+                    state
+                }
         return FlowableLifecycle(flowable)
+    }
+
+    private fun List<Lifecycle>.unregisterConnectivityBroadcastReceiver(state: LifecycleState) {
+        if (state == LifecycleState.Completed) {
+            val connectivityClazz: Class<*>? = Class.forName(CONNECTIVITY_ON_LIFECYCLE_CLASS_NAME)
+            connectivityClazz?.let {
+                val connectivityOnLifecycle: Lifecycle? = this.find { connectivityClazz.isInstance(it) }
+                connectivityOnLifecycle?.let {
+                    val obj: Any? = connectivityClazz.cast(connectivityOnLifecycle)
+                    val method: Method? = connectivityClazz.getMethod(UNREGISTER_CONNECTIVITY_BROADCAST_RECEIVER_METHOD_NAME)
+                    if (obj != null && method != null) {
+                        method.invoke(obj)
+                    }
+                }
+            }
+        }
     }
 }
