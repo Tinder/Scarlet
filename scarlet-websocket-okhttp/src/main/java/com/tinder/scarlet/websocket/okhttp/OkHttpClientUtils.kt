38b32cf110f1bc5d769544db5175565eaa8816ff
Roger Iyengar <ri@rogeriyengar.com>
2019-12-04 11:50:20 -0500

    Add cleartext traffic check

--- scarlet-websocket-okhttp/src/main/java/com/tinder/scarlet/websocket/okhttp/OkHttpClientUtils.kt
+++ scarlet-websocket-okhttp/src/main/java/com/tinder/scarlet/websocket/okhttp/OkHttpClientUtils.kt
@@ -10,11 +10,23 @@ import com.tinder.scarlet.WebSocket
 import com.tinder.scarlet.websocket.okhttp.request.RequestFactory
 import com.tinder.scarlet.websocket.okhttp.request.StaticUrlRequestFactory
 import okhttp3.OkHttpClient
+import java.lang.RuntimeException
 
 fun OkHttpClient.newWebSocketFactory(requestFactory: RequestFactory): WebSocket.Factory {
     return OkHttpWebSocket.Factory(OkHttpClientWebSocketConnectionEstablisher(this, requestFactory))
 }
 
 fun OkHttpClient.newWebSocketFactory(url: String): WebSocket.Factory {
+    if (url.startsWith("ws://")) {
+        try {
+            if (android.os.Build.VERSION.SDK_INT >= 23 &&
+                    !NetworkSecurityPolicy.getInstance().isCleartextTrafficPermitted()) {
+                throw RuntimeException("Android configuration does not permit cleartext traffic.")
+            }
+        } catch (_: ClassNotFoundException) {
+            // Not running on Android
+        }
+    }
+
     return newWebSocketFactory(StaticUrlRequestFactory(url))
 }
