6af4ab412039320b1c57c47eb27bda44f0ebacf0
Ihor Kushnirenko <ihor.kushnirenko@gmail.com>
2021-07-29 02:07:52 +0300

    Update Coroutines and remove Channels support

--- scarlet-stream-adapter-coroutines/src/main/java/com/tinder/streamadapter/coroutines/CoroutinesStreamAdapterFactory.kt
+++ scarlet-stream-adapter-coroutines/src/main/java/com/tinder/streamadapter/coroutines/CoroutinesStreamAdapterFactory.kt
@@ -6,7 +6,6 @@ package com.tinder.streamadapter.coroutines
 
 import com.tinder.scarlet.StreamAdapter
 import com.tinder.scarlet.utils.getRawType
-import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.flow.Flow
 import java.lang.reflect.Type
 
@@ -18,7 +17,6 @@ class CoroutinesStreamAdapterFactory : StreamAdapter.Factory {
     override fun create(type: Type): StreamAdapter<Any, Any> {
         return when (type.getRawType()) {
             Flow::class.java -> FlowStreamAdapter()
-            ReceiveChannel::class.java -> ReceiveChannelStreamAdapter()
             else -> throw IllegalArgumentException()
         }
     }
