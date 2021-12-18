430235dc1ba04d75019cc6929cf5707ff78cf115
Greg Giacovelli <greg.giacovelli@gotinder.com>
2021-12-17 19:05:36 -0800

    ChannelForwarder now calls dispose correctly on Completed and Error

--- scarlet-stream-adapter-coroutines/src/main/java/com/tinder/streamadapter/coroutines/ChannelForwarder.kt
+++ scarlet-stream-adapter-coroutines/src/main/java/com/tinder/streamadapter/coroutines/ChannelForwarder.kt
@@ -8,18 +8,21 @@ import kotlinx.coroutines.channels.sendBlocking
 internal class ChannelForwarder<T> : Stream.Observer<T> {
     private val _channel = Channel<T>()
     val channel: ReceiveChannel<T> = _channel
+    private var disposable: Stream.Disposable? = null
 
     fun start(stream: Stream<T>): ReceiveChannel<T> {
-        stream.start(this)
+        disposable = stream.start(this)
         return channel
     }
 
     override fun onComplete() {
         _channel.close()
+        disposable?.dispose()
     }
 
     override fun onError(throwable: Throwable) {
         _channel.close(throwable)
+        disposable?.dispose()
     }
 
     override fun onNext(data: T) {
